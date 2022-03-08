549dc3185557ad2dd3f653ef560338072c7fe8a5
Rohit Multani <rohitmultani153@gmail.com>
2022-03-08 20:32:50 +0530

    content updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,27 @@
 import React,{useState,useEffect} from 'react'
 import classes from './NavBar.module.css';
 import menu from '../Images/menu.svg'
+import genre from '../Images/genre.svg'
+import lang from '../Images/language.svg'
+import channel from '../Images/channel.svg'
 import login from '../Images/login.png'
 import disney from '../Images/Disney (2).svg'
 import List from '../listItems/List'
 const NavBar =()=>{
     const [focus,setFocus]=useState(false);
+    const [loggedIn,setLoggedIn] = useState(false);
     const focusHandler=()=>{setFocus(true)};
     return (<nav className={classes.NavBar}>
            <ul className={classes.sectionFirstList}>
-                <li><img src={menu} alt="menu" className={classes.menu}/></li>
+                <li className={classes.menuBar}><img src={menu} alt="menu" className={classes.menu}/>
+                <ul className={classes.hiddenMenu }>
+            <li>
+                <img src={channel} alt="channel"/>
+                Channels</li>
+            <li><img src={lang} alt="lang"/>Languages</li>
+            <li><img src={genre} alt="genre"/>Genre</li>
+        </ul>
+                </li>
                 <img src={disney} alt="menu" className={classes.menuLogo}/>
                 <List/>
             </ul>
@@ -18,11 +30,16 @@ const NavBar =()=>{
                     <form><input type="search" placeholder="Search" className={classes.search} onFocus={focusHandler}></input></form>
                 </li>
                 <li className={classes.profile}><img src={login} alt="menu" className={classes.menu}/>
-                <ul className={classes.hiddenProfile }>
+                {loggedIn?(<ul className={classes.hiddenProfile }>
             <li>WatchList</li>
             <li>My Account</li>
             <li>Logout</li>
-        </ul>
+        </ul>):(
+            <ul className={classes.hiddenProfile}>
+                <li>Login</li>
+                </ul>
+        )}
+                
                 </li>
             </ul>
     </nav>)
