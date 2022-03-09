43c7474e150a72c047d89c72a5d9ceff08e62424
Rohit Multani <rohitmultani153@gmail.com>
2022-03-10 00:30:41 +0530

    optimised

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,50 +1,50 @@
-import React,{useState,useEffect} from 'react'
-import classes from './NavBar.module.css';
-import menu from '../Images/menu.svg'
-import genre from '../Images/genre.svg'
-import lang from '../Images/language.svg'
-import channel from '../Images/channel.svg'
-import login from '../Images/login.png'
-import search from '../Images/search.svg'
-import disney from '../Images/Disney (2).svg'
-import List from '../listItems/List'
-const NavBar =()=>{
-    const [focus,setFocus]=useState(false);
-    const [loggedIn,setLoggedIn] = useState(false);
-    const focusHandler=()=>{setFocus(true)};
-    return (<nav className={classes.NavBar}>
-           <ul className={classes.sectionFirstList}>
-                <li className={classes.menuBar}><img src={menu} alt="menu" className={classes.menu}/>
-                <ul className={classes.hiddenMenu }>
-            <li>
-                <img src={search} alt="channel"/>
-                Channels</li>
-            <li><img src={lang} alt="lang"/>Languages</li>
-            <li><img src={genre} alt="genre"/>Genre</li>
-        </ul>
-                </li>
-                <img src={disney} alt="menu" className={classes.menuLogo}/>
-                <List/>
-            </ul>
-            <ul className={classes.sectionSecondList}>
-                <li>
-                    <form><input type="search" placeholder="Search" className={classes.search}/>
-                    <img src={search} alt="search" className={classes.searchIcon}></img>
-                </form>
-                </li>
-                <li className={classes.profile}><img src={login} alt="menu" className={classes.menu}/>
-                {loggedIn?(<ul className={classes.hiddenProfile }>
-            <li>WatchList</li>
-            <li>My Account</li>
-            <li>Logout</li>
-        </ul>):(
-            <ul className={classes.hiddenProfile}>
-                <li>Login</li>
-                </ul>
-        )}
-                
-                </li>
-            </ul>
-    </nav>)
+import React, { useState, useEffect } from "react";
+import classes from "./NavBar.module.css";
+import menu from "../Images/menu.svg";
+import login from "../Images/login.png";
+import search from "../Images/search.svg";
+import disney from "../Images/Disney (2).svg";
+import List from "../listItems/List";
+import HiddenMenu from "./HiddenComponents/HiddenMenu";
+import HiddenProfile from "./HiddenComponents/HiddenProfile";
+import HiddenLogin from "./HiddenComponents/HiddenLogin";
+const NavBar = () => {
+  const [focus, setFocus] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const focusHandler = () => {
+    setFocus(true);
+  };
+  return (
+    <nav className={classes.NavBar}>
+      <ul className={classes.sectionFirstList}>
+        <li className={classes.menuBar}>
+          <img src={menu} alt="menu" className={classes.menu} />
+          <HiddenMenu />
+        </li>
+        <img src={disney} alt="menu" className={classes.menuLogo} />
+        <List />
+      </ul>
+      <ul className={classes.sectionSecondList}>
+        <li>
+          <form>
+            <input
+              type="search"
+              placeholder="Search"
+              className={classes.search}
+            />
+            <img src={search} alt="search" className={classes.searchIcon}></img>
+          </form>
+        </li>
+        <li className={classes.profile}>
+          <img src={login} alt="menu" className={classes.menu} />
+          {loggedIn ? (
+            <HiddenProfile />
+          ) : (
+            <HiddenLogin/>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
 };
 export default NavBar;
