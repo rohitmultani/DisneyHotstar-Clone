0aaf35b34db1fc39840b5f05feae86052cc5ea16
Rohit Multani <rohitmultani153@gmail.com>
2022-03-11 21:09:23 +0530

    logout authenticated

--- src/components/HiddenComponents/HiddenProfile.js
+++ src/components/HiddenComponents/HiddenProfile.js
@@ -1,9 +1,16 @@
 import classes from '../NavBar.module.css';
-const HiddenProfile=()=>{
+import {auth} from "../../Firebase"
+const HiddenProfile=(props)=>{
+    const logout =()=>{
+        auth.signOut();
+        props.logoutHandler(false);
+        console.log("true")
+
+    }
     return(<ul className={classes.hiddenProfile }>
         <li>WatchList</li>
         <li>My Account</li>
-        <li>Logout</li>
+        <li onClick={logout}>Logout</li>
     </ul>)
 };
 export default HiddenProfile;
