9870ae416f7d8260a012a7c308d3ca33a8a317df
Rohit Multani <rohitmultani153@gmail.com>
2022-03-12 21:49:17 +0530

    login modal optimised

--- src/components/modal/LoginModal.js
+++ src/components/modal/LoginModal.js
@@ -1,30 +1,45 @@
-import { auth,provider } from "../../Firebase";
+import { auth, provider } from "../../Firebase";
 import cancel from "../../Images/cancel.svg";
 import classes from "./LoginModal.module.css";
+import React, { useState } from "react";
+import InitialModal from "./InitialModal";
+import ModalBar from "./ModalBar";
+import PhoneModal from "./PhoneModal";
+import Otp from "./Otp";
+
 const LoginModal = (props) => {
-  const LoginHandler=()=>{
-    auth.signInWithPopup(provider)
-  .then((res)=>{
-    console.log(res.additionalUserInfo.profile.name);
-    props.modalOpen(false);
-  })
-  .catch((err)=>{
-    console.log(err)
-  })
-  }
+  const [status, setStatus] = useState(false);
+  const [newState, setNewState] = useState(true);
+  const [digit, setDigit] = useState("");
+  const phoneHandler = (value, num) => {
+    // props.modalOpen(value)
+    setStatus(value);
+    setDigit(num);
+  };
+  const modalHandler = (para) => {
+    props.modalOpen(para);
+  };
+  const returnHandler = (value) => {
+    setStatus(value);
+  };
+  const nextHandler = (value) => {
+    setNewState(value);
+  };
   return (
-    <div className={classes.modalBar}>
-      <div
-        className={classes.cancelImage}
-        onClick={() => {
-          props.modalOpen(false);
-        }}
-      >
-        <img src={cancel} alt="cancel"></img>
-      </div>
-      <div className={classes.modalText}>Login to continue</div>
-      <div className={classes.modalSign} onClick={LoginHandler}>Sign In with Google</div>
-    </div>
+    <ModalBar>
+      {!status ? (
+        <InitialModal phoneOpen={phoneHandler} modalOpen={modalHandler} />
+      ) : newState ? (
+        <PhoneModal
+          digit={digit}
+          check={returnHandler}
+          next={nextHandler}
+          modalOpen={modalHandler}
+        />
+      ) : (
+        <Otp modalOpen={modalHandler} prev={nextHandler} />
+      )}
+    </ModalBar>
   );
 };
 export default LoginModal;
