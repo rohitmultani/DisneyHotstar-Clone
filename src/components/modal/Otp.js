37de336c19f274b64175af1244896c91b95c4340
Rohit Multani <rohitmultani153@gmail.com>
2022-03-12 21:51:10 +0530

    Otp section

---
+++ src/components/modal/Otp.js
@@ -0,0 +1,118 @@
+import React, { Fragment, useState,useEffect,useRef} from "react";
+import cancel from "../../Images/cancel.svg";
+import arrow from "../../Images/arrow.svg";
+import classes from "./LoginModal.module.css";
+const Otp = (props) => {
+  const [disable,setDisable]=useState(true);
+  const otp=useRef({otp1: "",
+  otp2: "",
+  otp3: "",
+  otp4: "",
+  otp5: "",
+  otp6: ""})
+  const changeHandler=(valueNew,event)=>{
+  
+    const newOtp=({[valueNew]:event.target.value})
+    otp.current={...otp.current,...newOtp}
+    if(valueNew==="otp6")
+    {
+      const otpAuth=otp.current.otp1 +otp.current.otp2+otp.current.otp3+otp.current.otp4+otp.current.otp5+otp.current.otp6;
+    console.log(otpAuth);
+    if(otpAuth.length===6){
+      setDisable(false)
+    }
+    else{
+      setDisable(true)
+    }
+    }
+}
+  const inputfocus = (elmnt) => {
+    if (elmnt.key === "Delete" || elmnt.key === "Backspace") {
+      const next = elmnt.target.tabIndex - 2;
+      if (next > -1) {
+        elmnt.target.previousSibling.focus();
+      }
+    } else {
+      const next = elmnt.target.tabIndex + 1;
+      if (next < 7) {
+        elmnt.target.nextSibling.focus();
+      }
+    }
+  };
+  const otpSubmit=(event)=>{
+    event.preventDefault();
+    const otpAuth=otp.current.otp1 +otp.current.otp2+otp.current.otp3+otp.current.otp4+otp.current.otp5+otp.current.otp6;
+    console.log(otpAuth);
+
+  };
+
+  const prevHandler=()=>{
+    props.check(false);
+  }
+  return (
+    <Fragment>
+      <div className={classes.imagesState}>
+        <img src={arrow} alt="prevState" onClick={()=>props.prev(true)}></img>
+        <img src={cancel} alt="cancel" onClick={()=>props.modalOpen(false)}></img>
+      </div>
+      <div className={classes.modalText}>
+        Enter the 6-digit code sent to +91******7990
+      </div>
+      <div className={classes.modalOtp}>
+        <input
+          type="text"
+          tabIndex="1"
+          maxLength="1"
+          ref={otp.current.opt1}
+          onChange={e=>changeHandler("otp1",e)}
+          onKeyUp={(e) => inputfocus(e)}
+          autoFocus
+        ></input>
+        <input
+          type="text"
+          tabIndex="2"
+          maxLength="1"
+          ref={otp.current.opt2}
+          onChange={e=>changeHandler("otp2",e)}
+          onKeyUp={(e) => inputfocus(e)}
+        ></input>
+        <input
+          type="text"
+          tabIndex="3"
+          maxLength="1"
+          ref={otp.current.opt3}
+          onChange={e=>changeHandler("otp3",e)}
+          onKeyUp={(e) => inputfocus(e)}
+        ></input>
+        <input
+          type="text"
+          tabIndex="4"
+          maxLength="1"
+          ref={otp.current.opt4}
+          onChange={e=>changeHandler("otp4",e)}
+          onKeyUp={(e) => inputfocus(e)}
+        ></input>
+        <input
+          type="text"
+          tabIndex="5"
+          maxLength="1"
+          ref={otp.current.opt5}
+          onChange={e=>changeHandler("otp5",e)}
+          onKeyUp={(e) => inputfocus(e)}
+        ></input>
+        <input
+          type="text"
+          tabIndex="6"
+          maxLength="1"
+          ref={otp.current.opt6}
+          onChange={e=>changeHandler("otp6",e)}
+          onKeyUp={(e) => inputfocus(e)}
+        ></input>
+      </div>
+      <button type="submit" className={classes.btn} onClick={otpSubmit} disabled={disable}>
+        CONTINUE
+      </button>
+    </Fragment>
+  );
+};
+export default Otp;
