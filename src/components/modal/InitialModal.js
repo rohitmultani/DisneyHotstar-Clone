5954cf35423f544f5282e3d809d845f4bd66055c
Rohit Multani <rohitmultani153@gmail.com>
2022-03-12 21:48:40 +0530

    modal updated

---
+++ src/components/modal/InitialModal.js
@@ -0,0 +1,39 @@
+import { Fragment,useState} from "react";
+import { auth,provider } from "../../Firebase";
+import cancel from "../../Images/cancel.svg";
+import classes from "./LoginModal.module.css"
+const InitialModal=(props)=>{
+
+    const LoginHandler=()=>{
+        auth.signInWithPopup(provider)
+      .then((res)=>{
+        console.log(res.additionalUserInfo.profile.name);
+        props.modalOpen(false);
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+      }
+      const mobilePhoneHandler=(event)=>{
+        props.phoneOpen(true,event.target.value)
+    }
+    return(
+        <Fragment>
+        <div
+        className={classes.cancelImage}
+        onClick={() => {
+          props.modalOpen(false);
+        }}
+      >
+        <img src={cancel} alt="cancel"></img>
+      </div>
+      <div className={classes.modalText}>Login to continue</div>
+      <div className={classes.modalSign} onClick={LoginHandler}>Sign In with Google</div>
+      <div className={classes.modalOption}>or</div>
+      <div className={classes.modalPhone}>
+        <span>+91 | </span>
+        <input type="tel" placeholder="Enter Your Mobile Number" min="0" maxLength="10" onChange={mobilePhoneHandler} autoFocus></input></div>
+        </Fragment>
+    )
+};
+export default InitialModal;
