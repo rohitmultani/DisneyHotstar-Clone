d4b7b3b23a2ee6cc22817127dc060f84e2582ed5
Rohit Multani <rohitmultani153@gmail.com>
2022-04-13 20:44:59 +0530

    optimised

--- src/components/modal/LoginModal.js
+++ src/components/modal/LoginModal.js
@@ -1,16 +1,29 @@
 import { auth, provider } from "../../Firebase";
 import cancel from "../../Images/cancel.svg";
 import classes from "./LoginModal.module.css";
-import React, { useState } from "react";
+import React, { useState ,useRef} from "react";
 import InitialModal from "./InitialModal";
 import ModalBar from "./ModalBar";
 import PhoneModal from "./PhoneModal";
 import Otp from "./Otp";
+import Welcome from './Welcome'
 
 const LoginModal = (props) => {
   const [status, setStatus] = useState(false);
   const [newState, setNewState] = useState(true);
+  const [loggedIn,setLoggedIn]=useState(true)
+  // const [password, setPass] = useState();
+  const [last4, setLast4] = useState();
+  const [phone,setPhone] = useState();
+  const [profName,setprofname]=useState("")
+  const password=useRef();
   const [digit, setDigit] = useState("");
+  auth.onAuthStateChanged((user)=>{
+    if(user){
+      return setLoggedIn(true);
+    }
+    setLoggedIn(false);
+  })
   const phoneHandler = (value, num) => {
     // props.modalOpen(value)
     setStatus(value);
@@ -25,20 +38,40 @@ const LoginModal = (props) => {
   const nextHandler = (value) => {
     setNewState(value);
   };
-  return (
+  const passwordHandler=(pass)=>{
+    password.current=pass;
+    console.log("parent",password.current)
+  }
+  const userPhone=(value)=>{
+setPhone(value);
+  }
+  const lastDigits =(value)=>{
+setLast4(value)
+  }
+  const profileHandler=(name)=>{
+setprofname(name)
+  }
+  return (  
     <ModalBar>
-      {!status ? (
-        <InitialModal phoneOpen={phoneHandler} modalOpen={modalHandler} />
+      {!loggedIn?(!status ? (
+        <InitialModal phoneOpen={phoneHandler} modalOpen={modalHandler} prof={profileHandler} />
       ) : newState ? (
         <PhoneModal
           digit={digit}
           check={returnHandler}
           next={nextHandler}
           modalOpen={modalHandler}
+          password={password.current}
+          digitLast={lastDigits}
         />
       ) : (
-        <Otp modalOpen={modalHandler} prev={nextHandler} />
-      )}
+        <Otp modalOpen={modalHandler}
+        phoneNum={last4}
+        prev={nextHandler} password={passwordHandler} 
+        userDetails={userPhone}
+        />
+      ))
+      :<Welcome phoneNum={phone} profile={profName}/>} 
     </ModalBar>
   );
 };
