0a13ffce8d77c2b346b441a1e84dbcd64f970d85
Rohit Multani <rohitmultani153@gmail.com>
2022-04-29 13:07:08 +0530

    updates props

--- src/components/Slider/LatestLayer.js
+++ src/components/Slider/LatestLayer.js
@@ -3,13 +3,19 @@ import classes from './LatestLayer.module.css';
 import Batman from "../../Images/batman.webp"
 import HiddenDetails from "../HiddenComponents/HiddenDetails";
 const LatestLayer=(props)=>{
+    const imAddress=`http://image.tmdb.org/t/p/w500/${props.image}`
     return(
         <SmallLayout>
             <div className={classes.SingleBox}> 
                 
-<img src={Batman} className={classes.batImg}></img>
+<img src={imAddress} className={classes.batImg}></img>
 <div className={classes.hiddenInfo}>
-    <HiddenDetails/>
+    <HiddenDetails 
+    id={props.id}
+    title={props.title}
+    detail={props.detail}
+    image={props.image}
+    />
 </div>
             </div>
         </SmallLayout>
