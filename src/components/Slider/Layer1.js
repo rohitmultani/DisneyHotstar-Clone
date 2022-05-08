f76857f9c06bccf939f5c07380c39f72d7f11e5c
Rohit Multani <rohitmultani153@gmail.com>
2022-05-08 16:42:22 +0530

    props added

--- src/components/Slider/Layer1.js
+++ src/components/Slider/Layer1.js
@@ -2,17 +2,18 @@ import SliderLayout from '../Layout/SliderLayout'
 import batman from '../../Images/batman.webp';
 import classes from './Layer1.module.css'
 const Layer1 =(props)=>{
+    const imAddress=`http://image.tmdb.org/t/p/w500/${props.image}`
     return(
         <SliderLayout>
             <div className={classes.layout}>
 
             <div className={classes.titleHead}>
             <div className={classes.title}>{props.title}</div>
-            <div className={classes.genre}>Action</div>
-            <div className={classes.description}>A grim, gritty, and gripping super-noir, The Batman ranks among the Dark Knight's bleakest -- and most thrillingly ambitious -- live-action outings.</div>
+            <div className={classes.genre}>{props.genre}</div>
+            <div className={classes.description}>{props.detail}</div>
             </div>
             
-<img src={batman} className={classes.img}></img>
+<img src={imAddress} className={classes.img}></img>
             
             </div>
         </SliderLayout>
