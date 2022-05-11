43744d01ac2d82a7ef2c1e39f88bcb174cf1b46b
Rohit Multani <rohitmultani153@gmail.com>
2022-05-11 08:22:18 +0530

    vdo embeded

---
+++ src/components/Pages/YoutubePlay/YoutubePlay.js
@@ -0,0 +1,23 @@
+import BigLayout from "../../Layout/BigLayout";
+import classes from "./YoutubePlay.module.css";
+const YoutubePlay =(props)=>{
+    return(
+        <BigLayout>        
+            <div>
+               
+                
+    <iframe
+      className={classes.frame}
+      src={`https://www.youtube.com/embed/${props.vdId}`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+      />
+
+      </div>
+      </BigLayout>
+
+    );
+}
+export default YoutubePlay;
