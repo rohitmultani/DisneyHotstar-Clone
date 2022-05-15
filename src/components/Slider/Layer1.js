1be2d83a8a50f51d5c9f33431a2f9e0b2976fe3d
Rohit Multani <rohitmultani153@gmail.com>
2022-05-15 13:17:47 +0530

    Layer1 with props

--- src/components/Slider/Layer1.js
+++ src/components/Slider/Layer1.js
@@ -1,22 +1,32 @@
-import SliderLayout from '../Layout/SliderLayout'
-import batman from '../../Images/batman.webp';
-import classes from './Layer1.module.css'
-const Layer1 =(props)=>{
-    const imAddress=`http://image.tmdb.org/t/p/w500/${props.image}`
-    return(
-        <SliderLayout>
-            <div className={classes.layout}>
+import SliderLayout from "../Layout/SliderLayout";
+import classes from "./Layer1.module.css";
+import PlayListContext from "../../Store/PlayList-context";
+import { useContext,memo} from "react";
+const Layer1 = (props) => {
+  const PlayCtx = useContext(PlayListContext);
+  const addHandler = (event) => {
+    PlayCtx.addMovies({
+      id: props.id,
+      title: props.title,
+      name: props.name,
+      image: props.image,
+      detail: props.detail,
+      vote:props.vote
+    });
+  };
+  const imAddress = `http://image.tmdb.org/t/p/w500/${props.image}`;
+  return (
+    <SliderLayout>
+      <div className={classes.layout} onClick={addHandler}>
+        <div className={classes.titleHead}>
+          <div className={classes.title}>{props.title}</div>
+          <div className={classes.genre}>{props.vote}</div>
+          <div className={classes.description}>{props.detail}</div>
+        </div>
 
-            <div className={classes.titleHead}>
-            <div className={classes.title}>{props.title}</div>
-            <div className={classes.genre}>{props.genre}</div>
-            <div className={classes.description}>{props.detail}</div>
-            </div>
-            
-<img src={imAddress} className={classes.img}></img>
-            
-            </div>
-        </SliderLayout>
-    )
-}
-export default Layer1;
+        <img src={imAddress} className={classes.img}></img>
+      </div>
+    </SliderLayout>
+  );
+};
+export default memo(Layer1);
