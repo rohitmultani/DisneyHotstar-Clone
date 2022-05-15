f57f388e85af87c8494849bd23219120cd27e861
Rohit Multani <rohitmultani153@gmail.com>
2022-05-15 12:39:22 +0530

    welcome enhanced

--- src/components/modal/Welcome.js
+++ src/components/modal/Welcome.js
@@ -1,16 +1,59 @@
 import classes from "./LoginModal.module.css";
-import {Fragment} from 'react'
+import { Fragment, useEffect, useState, memo } from "react";
+import { auth, provider } from "../../Firebase";
+import { Link } from "react-router-dom";
+import NavBar from "../NavBar";
+const Welcome = (props) => {
+  const [email, setEmail] = useState();
+  const [Image, setImage] = useState();
+  const [profile, setProfile] = useState();
+  const [phone, setPhone] = useState();
+  const [show, setShow] = useState(false);
+  const showHandler = () => {
+    setShow(true);
+  };
+  useEffect(() => profileHandler, [show]);
+  function profileHandler() {
+    const user = auth.currentUser;
+    console.log(user);
 
-const Welcome=(props)=>{
-    return(
-        <Fragment>
-            
-        <div className={classes.welcome}>
-            Welcome
+    if (user !== null) {
+      user.providerData.forEach((profile) => {
+        setTimeout(() => {
+          setProfile(profile.displayName);
+          setEmail(profile.email);
+          setPhone(profile.phoneNumber);
+          setImage(profile.photoURL);
+        }, 100);
+      });
+    }
+  }
+  return (
+    <Fragment>
+      <NavBar />
+      {!show ? (
+        <div>
+          <div className={classes.modalSign2} onClick={showHandler}>
+            View Profile
+          </div>
+          <Link to="/WatchList" className={classes.link}>
+            <div className={classes.modalSign2}>View WatchList</div>
+          </Link>
         </div>
-        <div className={classes.phone}>{props.phoneNum}</div>
-        <div className={classes.phone}>{props.profile}</div>
-        </Fragment>
-    );
-}
-export default Welcome;
+      ) : (
+        <div>
+          <div className={classes.welcome} onClick={profileHandler}>
+            Welcome
+          </div>
+          <div className={classes.phone}>{phone}</div>
+          <div className={classes.profileImage}>
+            <img src={Image}></img>
+          </div>
+          <div className={classes.phone}>{profile}</div>
+          <div className={classes.email}>{email}</div>
+        </div>
+      )}
+    </Fragment>
+  );
+};
+export default memo(Welcome);
