20b2b08139fd9419e86863f302bf627ddc215c24
Rohit Multani <rohitmultani153@gmail.com>
2022-05-15 12:22:13 +0530

    memo and props added

--- src/components/LayoutDesigned/Design2.js
+++ src/components/LayoutDesigned/Design2.js
@@ -1,19 +1,34 @@
-import LandScape from '../Layout/LandScape';
-import classes from './Design2.module.css';
-import batMan from '../../Images/batman.webp'
-import HiddenAbout from '../HiddenComponents/HiddenAbout';
-const Design2 =(props)=>{
-    const imAddress=`http://image.tmdb.org/t/p/w500/${props.image}`
-    return(
-        <LandScape>
-              <div className={classes.SingleBox}> 
-            <img src={imAddress} className={classes.im}></img>
-            <div className={classes.hiddenInfo}>
-    <HiddenAbout title={props.title} id={props.id} detail={props.detail } name={props.name}/>
-</div>
-</div>
-</LandScape>
-    );
-
-}
-export default Design2;
+import LandScape from "../Layout/LandScape";
+import classes from "./Design2.module.css";
+import { useContext, memo } from "react";
+import PlayListContext from "../../Store/PlayList-context";
+import HiddenAbout from "../HiddenComponents/HiddenAbout";
+const Design2 = (props) => {
+  const PlayCtx = useContext(PlayListContext);
+  const addHandler = () => {
+    PlayCtx.addMovies({
+      id: props.id,
+      title: props.title,
+      name: props.name,
+      image: props.image,
+      detail: props.detail,
+    });
+  };
+  const imAddress = `http://image.tmdb.org/t/p/w500/${props.image}`;
+  return (
+    <LandScape>
+      <div className={classes.SingleBox} onClick={addHandler}>
+        <img src={imAddress} className={classes.im}></img>
+        <div className={classes.hiddenInfo}>
+          <HiddenAbout
+            title={props.title}
+            id={props.id}
+            detail={props.detail}
+            name={props.name}
+          />
+        </div>
+      </div>
+    </LandScape>
+  );
+};
+export default memo(Design2);
