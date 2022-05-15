75cf378e5d5186c3ea0a6ec4bd704689c9267778
Rohit Multani <rohitmultani153@gmail.com>
2022-05-15 10:48:48 +0530

    link to channel

--- src/components/HiddenComponents/HiddenMenu.js
+++ src/components/HiddenComponents/HiddenMenu.js
@@ -1,15 +1,14 @@
 import classes from '../NavBar.module.css';
-import genre from '../../Images/genre.svg'
-import lang from '../../Images/language.svg'
+import { memo } from "react";
+import {Link} from 'react-router-dom'
 import channel from '../../Images/channel.svg'
 const HiddenMenu =()=>{
     return(
         <ul className={classes.hiddenMenu}>
-                    <li>
-                        <img src={channel} alt="channel"/>
-                        Channels</li>
-                    <li><img src={lang} alt="lang"/>Languages</li>
-                    <li><img src={genre} alt="genre"/>Genre</li>
+                    
+                    <Link to="/channel" className={classes.Link}>  <li> <img src={channel} alt="channel"/>
+                        Channels</li></Link>
+
                 </ul>)
 };
-export default HiddenMenu
+export default memo(HiddenMenu);
