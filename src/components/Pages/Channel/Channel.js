d14da0e1f634079b7a1ac9d5242a5afba23d972c
Rohit Multani <rohitmultani153@gmail.com>
2022-05-15 13:22:09 +0530

    channels api added

--- src/components/Pages/Channel/Channel.js
+++ src/components/Pages/Channel/Channel.js
@@ -1,29 +1,45 @@
-import  Navbar  from "../../NavBar"
-import classes from './Channel.module.css'
-import { Fragment } from "react";
+import Navbar from "../../NavBar";
+import classes from "./Channel.module.css";
+import { Fragment, useState, useEffect, memo } from "react";
 import Design1 from "../../LayoutDesigned/Design1";
 
-const Channel=()=>{
-    return(<Fragment>
-        <Navbar/>
-        <div className={classes.pageName}>Channels</div>
-        <div className={classes.containerList}>
-            <Design1/>
-            <Design1/>
-            <Design1/>
-            <Design1/>
-            <Design1/>
-            <Design1/>
-            <Design1/>
-            <Design1/>
-            <Design1/>
-            <Design1/>
-            <Design1/>
-            <Design1/>
-            <Design1/>
-            <Design1/>
-        </div>
-    </Fragment>);
+const Channel = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, []);
+  function fetchMoviesHandler() {
+    const newLocal = `https://api.themoviedb.org/3/watch/providers/movie?api_key=8936bfb1497ed6daf448de63b7b6cb7c`;
+    fetch(newLocal)
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((movieData) => {
+          console.log(movieData);
 
-}
-export default Channel;
+          return {
+            id: movieData.provider_id,
+            name: movieData.provider_name,
+            image: movieData.logo_path,
+          };
+        });
+        setMovies(transformedMovies);
+      });
+  }
+  return (
+    <Fragment>
+      <Navbar />
+      <div className={classes.pageName}>Channels</div>
+      <div className={classes.containerList}>
+        {movies.map((movie) => (
+          <div key={movie.id} className={classes.movieItem}>
+            <Design1 id={movie.id} name={movie.name} image={movie.image} />
+          </div>
+        ))}
+      </div>
+    </Fragment>
+  );
+};
+export default memo(Channel);
