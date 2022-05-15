f27e55f9e5690ea8d4b27846dfd5fb79be78b634
Rohit Multani <rohitmultani153@gmail.com>
2022-05-15 13:29:47 +0530

    tv page

--- src/components/Pages/Tv/Tv.js
+++ src/components/Pages/Tv/Tv.js
@@ -1,51 +1,46 @@
-import React,{useState,useEffect, useImperativeHandle} from 'react';
-import { Fragment } from 'react';
-import classes from '../Search/Search.module.css';
-import {Link,useParams} from 'react-router-dom';
+import React, { useState, useEffect} from "react";
+import { Fragment,memo} from "react";
+import classes from "../Search/Search.module.css";
+import { Link, useParams } from "react-router-dom";
 
-import LatestLayer from '../../Slider/LatestLayer';
-import NavBar from '../../NavBar';
-const Tv=(props)=>{
-    const params=useParams();
-    const [movies, setMovies] = useState([]);
-    useEffect(() => {
-        fetchMoviesHandler();
-      }, [params.any]);
-      
-    function fetchMoviesHandler() {
-        console.log(params.any)
-        const newLocal =
-          `https://api.themoviedb.org/3/tv/${params.any}?api_key=8936bfb1497ed6daf448de63b7b6cb7c`;
-        fetch(newLocal)
-          .then((response) => {
-              console.log(response)
-              return response.json();
-            })
-            .then((data) => {
-                
-                const transformedMovies = data.results.map((movieData) => {
-                console.log(movieData)
-            
-            
-              return {
-                id: movieData.id,
-                name: movieData.name,
-                image: movieData.poster_path,
-                detail: movieData.overview,
-                vote:movieData.vote_average
-              };
-            });
-            setMovies(transformedMovies);
-          });
-          
-        }
-    return(
-        <Fragment>
-            <NavBar/>
-            <div className={classes.Latest}>{params.any}</div>
-            <div className={classes.movieItem}>
-        
-          {movies.map((movie) => (
+import LatestLayer from "../../Slider/LatestLayer";
+import NavBar from "../../NavBar";
+const Tv = (props) => {
+  const params = useParams();
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [params.any]);
+
+  function fetchMoviesHandler() {
+    console.log(params.any);
+    const newLocal = `https://api.themoviedb.org/3/tv/${params.any}?api_key=8936bfb1497ed6daf448de63b7b6cb7c`;
+    fetch(newLocal)
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((movieData) => {
+          console.log(movieData);
+
+          return {
+            id: movieData.id,
+            name: movieData.name,
+            image: movieData.poster_path,
+            detail: movieData.overview,
+            vote: movieData.vote_average,
+          };
+        });
+        setMovies(transformedMovies);
+      });
+  }
+  return (
+    <Fragment>
+      <NavBar />
+      <div className={classes.Latest}>{params.any}</div>
+      <div className={classes.movieItem}>
+        {movies.map((movie) => (
           <div key={movie.id} className={classes.movieItem}>
             <Link to={`/Play/${movie.id}`} className={classes.link}>
               <LatestLayer
@@ -60,8 +55,8 @@ const Tv=(props)=>{
             </Link>
           </div>
         ))}
-        </div>
-        </Fragment>
-    );
-    }
-    export default Tv;
+      </div>
+    </Fragment>
+  );
+};
+export default memo(Tv);
