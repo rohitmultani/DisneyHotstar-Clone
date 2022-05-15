f6807b04bb38d982d5e4300e56a8749099e92328
Rohit Multani <rohitmultani153@gmail.com>
2022-05-15 13:27:46 +0530

    search bar added

---
+++ src/components/Pages/Search/Search.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect, memo } from "react";
+import LatestSlider from "../../Slider/LatestSlider";
+import { Fragment } from "react";
+import classes from "./Search.module.css";
+import SearchNav from "./SearchNav";
+import { Link } from "react-router-dom";
+import sadFace from "../../../Images/sad.svg";
+import LatestLayer from "../../Slider/LatestLayer";
+const Search = (props) => {
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("");
+  const [loaded, setLoaded] = useState(true);
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [query]);
+  const queryHandler = (e) => {
+    setQuery(e);
+    console.log(query);
+  };
+  function fetchMoviesHandler() {
+    const newLocal = `https://api.themoviedb.org/3/search/movie?api_key=8936bfb1497ed6daf448de63b7b6cb7c&query=${query}&page=1`;
+    fetch(newLocal)
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        if (data.total_pages === 0) {
+          setLoaded(false);
+        } else setLoaded(true);
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.id,
+            title: movieData.title,
+            image: movieData.poster_path,
+            detail: movieData.overview,
+            genres: movieData.genre_ids[0],
+          };
+        });
+        setMovies(transformedMovies);
+      });
+  }
+  return (
+    <Fragment>
+      <SearchNav search={queryHandler} />
+      <div className={classes.movieItem}>
+        {loaded ? (
+          movies.map((movie) => (
+            <div key={movie.id} className={classes.movieItem}>
+              <Link to={`/Play/${movie.id}`} className={classes.link}>
+                <LatestLayer
+                  id={movie.id}
+                  //  key={movie.id}
+                  name={movie.name}
+                  title={movie.title}
+                  image={movie.image}
+                  detail={movie.detail}
+                />
+              </Link>
+            </div>
+          ))
+        ) : (
+          <div className={classes.found}>
+            Not Found
+            <img src={sadFace}></img>
+          </div>
+        )}
+      </div>
+    </Fragment>
+  );
+};
+export default memo(Search);
