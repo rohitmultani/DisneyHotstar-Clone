f8341742c91c44ad8f7f6729547a4b35bd1dc103
Rohit Multani <rohitmultani153@gmail.com>
2022-05-15 13:29:02 +0530

    search navbar

---
+++ src/components/Pages/Search/SearchNav.js
@@ -0,0 +1,28 @@
+import classes from './Search.module.css';
+import disney from "../../../Images/Disney (2).svg";
+import search from "../../../Images/search.svg";
+import {memo} from 'react'
+import { Link } from 'react-router-dom';
+const SearchNav=(props)=>{
+    const displayHandler=(e)=>{
+        props.search(e.target.value)
+    }
+    return(
+        <nav className={classes.NavBar}>
+            <div className={classes.home}> 
+            <Link to="/">
+            <img src={disney} alt="menu" className={classes.menuLogo} />
+            </Link>
+            </div>
+            <div className={classes.searchHandle}> 
+            <input
+              type="search"
+              placeholder="Search"
+              className={classes.search}
+              onChange={displayHandler}
+            />
+            </div>
+        </nav>
+    );
+    }
+    export default memo(SearchNav);
