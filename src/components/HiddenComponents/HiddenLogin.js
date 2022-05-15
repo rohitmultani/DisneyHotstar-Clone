57b059963c2d4210cedeca3825a0a01caa25e468
Rohit Multani <rohitmultani153@gmail.com>
2022-05-15 13:52:10 +0530

    final commit

--- src/components/HiddenComponents/HiddenLogin.js
+++
@@ -1,7 +0,0 @@
-import classes from "../NavBar.module.css";
-const HiddenLogin=()=>{
-    return(<ul className={classes.hiddenProfile}>
-        <li>Login</li>
-      </ul>)
-};
-export default HiddenLogin;
