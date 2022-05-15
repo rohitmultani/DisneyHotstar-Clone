410aaaa25b05928260989091a0f30e62472b9312
Rohit Multani <rohitmultani153@gmail.com>
2022-05-15 12:24:47 +0530

    new design added

---
+++ src/components/LayoutDesigned/Design3.js
@@ -0,0 +1,45 @@
+import { useState, memo } from "react";
+import classes from "./Design3.module.css";
+import BigLayout from "../Layout/BigLayout";
+import YoutubePlay from "../Pages/YoutubePlay/YoutubePlay";
+const Design3 = (props) => {
+  const [movieId, setMovieId] = useState();
+  const [play, setPlay] = useState(false);
+  const imAddress = `http://image.tmdb.org/t/p/w500/${props.image}`;
+  const trailerHandler = () => {
+    let Local;
+    if (props.title) {
+      Local = `https://api.themoviedb.org/3/movie/${props.id}/videos?api_key=8936bfb1497ed6daf448de63b7b6cb7c`;
+    } else if (props.name) {
+      Local = `https://api.themoviedb.org/3/tv/${props.id}/videos?api_key=8936bfb1497ed6daf448de63b7b6cb7c`;
+    }
+
+    fetch(Local)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setPlay(true);
+        const transformedVideo = data.results.map((movieData) => {
+          return {
+            key: movieData.key,
+          };
+        });
+
+        setMovieId(transformedVideo[0].key);
+      });
+  };
+
+  return (
+    <BigLayout>
+      <div className={classes.SingleBox}>
+        <img src={imAddress} className={classes.im}></img>
+        <button className={classes.watchBtn} onClick={trailerHandler}>
+          Watch Trailer
+        </button>
+        {play && <YoutubePlay vdId={movieId} />}
+      </div>
+    </BigLayout>
+  );
+};
+export default memo(Design3);
