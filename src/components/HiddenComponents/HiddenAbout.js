abc3946761a59f6e610363d9b398fcb7c0c87fbd
Rohit Multani <rohitmultani153@gmail.com>
2022-05-15 10:46:32 +0530

    routing added

--- src/components/HiddenComponents/HiddenAbout.js
+++ src/components/HiddenComponents/HiddenAbout.js
@@ -1,21 +1,22 @@
 import classes from '../LayoutDesigned/Design2.module.css';
-import { useContext } from 'react';
+import { useContext,memo } from 'react';
+import {Link} from 'react-router-dom'
 import WatchListContext from '../../Store/WatchList-context';
 const HiddenAbout=(props)=>{
     const WatchCtx=useContext(WatchListContext);
     const removeHandler=()=>{
-        console.log(props.id)
-        WatchCtx.removeMovies({
-            id:props.id
-        })
+        console.log(props)
+        WatchCtx.removeMovies(
+            props.id
+        )
     }
     return(
         <div className={classes.detailss}>
-            <div className={classes.Title}>{props.title}</div>
-            <div className={classes.Explane}>A grim, gritty, and gripping super-noir, The Batman ranks among the Dark Knight's bleakest -- and most thrillingly ambitious -- live-action outings.</div>
-            <div className={classes.watchlist}  onClick={removeHandler}>REMOVE FROM WATCHLIST</div>
+            <div className={classes.Title}>{props.title} {props.name}</div>
+            <div className={classes.Explane}>{props.detail}</div>
+          <Link to="/WatchList">  <div className={classes.watchlist}  onClick={removeHandler}>REMOVE FROM WATCHLIST</div></Link>
         </div>
     );
 
 }
-export default HiddenAbout;
+export default memo(HiddenAbout);
