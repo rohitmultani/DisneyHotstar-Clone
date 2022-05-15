64a41b90db9686d6f2d43ebb575724932c27f6b3
Rohit Multani <rohitmultani153@gmail.com>
2022-05-15 13:30:33 +0530

    memo

--- src/components/Pages/WatchList/WatchList.js
+++ src/components/Pages/WatchList/WatchList.js
@@ -1,39 +1,44 @@
-import  Navbar  from "../../NavBar"
-import classes from './WatchList.module.css'
-import { Fragment } from "react";
+import Navbar from "../../NavBar";
+import classes from "./WatchList.module.css";
+import { Fragment, memo } from "react";
 import Design2 from "../../LayoutDesigned/Design2";
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import WatchListContext from "../../../Store/WatchList-context";
 
-const WatchList=(props)=>{
-    const WatchCtx=useContext(WatchListContext);
-    const movieAddHandler = (movies) => {};
-    const movieRemoveHandler = (id) => {
-        WatchCtx.removeMovies(id);
-    };
-    // console.log("hello from watchlist")
-    // console.log(WatchCtx.movies.map((movies)=>console.log(movies.id))) 
-    const WatchItems = (
-        <ul className={classes.containerList}>
-          {WatchCtx.movies.map((item) => (
-            <Design2
-                id={item.id }
-                key={item.id}
-                title={item.title}
-                image={item.image}
-                onRemove={movieRemoveHandler.bind(null, item.id)}
-              onAdd={movieAddHandler.bind(null, item)}
-            />
-          ))}
-        </ul>
-      );
-    
-        return(<Fragment>
-        <Navbar/>
-        <div className={classes.pageName}>WatchList</div>
-      {WatchItems}
-      
-    </Fragment>);
+const WatchList = (props) => {
+  const WatchCtx = useContext(WatchListContext);
+  const movieAddHandler = (movies) => {};
+  const movieRemoveHandler = (id) => {
+    WatchCtx.removeMovies(id);
+  };
 
-}
-export default WatchList;
+  const WatchItems = (
+    <ul className={classes.containerList}>
+      {WatchCtx.movies.map((item) => (
+        <Link to={`/Play/${item.id}`}>
+          <Design2
+            id={item.id}
+            key={item.id}
+            name={item.name}
+            detail={item.detail}
+            title={item.title}
+            image={item.image}
+            vote={item.gName}
+            onRemove={movieRemoveHandler.bind(null, item.id)}
+            onAdd={movieAddHandler.bind(null, item)}
+          />
+        </Link>
+      ))}
+    </ul>
+  );
+
+  return (
+    <Fragment>
+      <Navbar />
+      <div className={classes.pageName}>WatchList</div>
+      {WatchItems}
+    </Fragment>
+  );
+};
+export default memo(WatchList);
