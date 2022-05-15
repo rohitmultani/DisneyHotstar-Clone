6ecbc5963d933bae527178ebf510f8963b03116c
Rohit Multani <rohitmultani153@gmail.com>
2022-05-15 13:12:11 +0530

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, memo } from "react";
 import classes from "./NavBar.module.css";
 import menu from "../Images/menu.svg";
 import login from "../Images/login.png";
@@ -8,25 +8,26 @@ import List from "../listItems/List";
 import HiddenMenu from "./HiddenComponents/HiddenMenu";
 import HiddenProfile from "./HiddenComponents/HiddenProfile";
 import Login from "./Login/Login";
-import {auth} from '../Firebase'
+import { auth } from "../Firebase";
+import { Link } from "react-router-dom";
 import LoginModal from "./modal/LoginModal";
 import BackDrop from "./modal/BackDrop";
 const NavBar = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [modal, setModal] = useState(false);
-  auth.onAuthStateChanged((user)=>{
-    if(user){
+  auth.onAuthStateChanged((user) => {
+    if (user) {
       return setLoggedIn(true);
     }
     setLoggedIn(false);
-  })
-  const modalHandler=()=>{
-    setModal(prevState=>(!prevState));
+  });
+  const modalHandler = () => {
+    setModal((prevState) => !prevState);
   };
-  const logoutHandler=(props)=>{
-setLoggedIn(props)
-  }
-  
+  const logoutHandler = (props) => {
+    setLoggedIn(props);
+  };
+
   return (
     <nav className={classes.NavBar}>
       <ul className={classes.sectionFirstList}>
@@ -34,26 +35,41 @@ setLoggedIn(props)
           <img src={menu} alt="menu" className={classes.menu} />
           <HiddenMenu />
         </li>
-        <img src={disney} alt="menu" className={classes.menuLogo} />
+        <Link to="/">
+          <img src={disney} alt="menu" className={classes.menuLogo} />
+        </Link>
         <List />
       </ul>
       <ul className={classes.sectionSecondList}>
         <li>
           <form>
-            <input
-              type="search"
-              placeholder="Search"
-              className={classes.search}
-            />
-            <img src={search} alt="search" className={classes.searchIcon}></img>
+            <Link to="/search">
+              <input
+                type="search"
+                placeholder="Search"
+                className={classes.search}
+              />
+              <img
+                src={search}
+                alt="search"
+                className={classes.searchIcon}
+              ></img>
+            </Link>
           </form>
         </li>
-        {loggedIn?(<li className={classes.profile}> <img src={login} alt="menu" className={classes.menu} />
-          <HiddenProfile logoutHandler={logoutHandler}/></li>):<Login modal={modalHandler}/>}
-          {modal&& <LoginModal modalOpen={modalHandler}/>}
-          {modal&& <BackDrop backdropOpen={modalHandler}/>}
+        {loggedIn ? (
+          <li className={classes.profile}>
+            {" "}
+            <img src={login} alt="menu" className={classes.menu} />
+            <HiddenProfile logoutHandler={logoutHandler} />
+          </li>
+        ) : (
+          <Login modal={modalHandler} />
+        )}
+        {modal && <LoginModal modalOpen={modalHandler} />}
+        {modal && <BackDrop backdropOpen={modalHandler} />}
       </ul>
     </nav>
   );
 };
-export default NavBar;
+export default memo(NavBar);
