5007ea2771ad9423677dea98881d162d48b5dddb
Rohit Multani <rohitmultani153@gmail.com>
2022-05-15 12:36:04 +0530

    welcome deleted

--- src/components/modal/LoginModal.js
+++ src/components/modal/LoginModal.js
@@ -1,29 +1,27 @@
 import { auth, provider } from "../../Firebase";
-import cancel from "../../Images/cancel.svg";
-import classes from "./LoginModal.module.css";
-import React, { useState ,useRef} from "react";
+import React, { useState, useRef, memo } from "react";
 import InitialModal from "./InitialModal";
 import ModalBar from "./ModalBar";
 import PhoneModal from "./PhoneModal";
 import Otp from "./Otp";
-import Welcome from './Welcome'
+import Welcome from "./Welcome";
 
 const LoginModal = (props) => {
   const [status, setStatus] = useState(false);
   const [newState, setNewState] = useState(true);
-  const [loggedIn,setLoggedIn]=useState(true)
+  const [loggedIn, setLoggedIn] = useState(true);
   // const [password, setPass] = useState();
   const [last4, setLast4] = useState();
-  const [phone,setPhone] = useState();
-  const [profName,setprofname]=useState("")
-  const password=useRef();
+  const [phone, setPhone] = useState("");
+  const [profName, setprofname] = useState("");
+  const password = useRef();
   const [digit, setDigit] = useState("");
-  auth.onAuthStateChanged((user)=>{
-    if(user){
+  auth.onAuthStateChanged((user) => {
+    if (user) {
       return setLoggedIn(true);
     }
     setLoggedIn(false);
-  })
+  });
   const phoneHandler = (value, num) => {
     // props.modalOpen(value)
     setStatus(value);
@@ -38,23 +36,30 @@ const LoginModal = (props) => {
   const nextHandler = (value) => {
     setNewState(value);
   };
-  const passwordHandler=(pass)=>{
-    password.current=pass;
-    console.log("parent",password.current)
-  }
-  const userPhone=(value)=>{
-setPhone(value);
-  }
-  const lastDigits =(value)=>{
-setLast4(value)
-  }
-  const profileHandler=(name)=>{
-setprofname(name)
-  }
-  return (  
+  const passwordHandler = (pass) => {
+    password.current = pass;
+    console.log("parent", password.current);
+  };
+  const userPhone = (value) => {
+    setPhone(value);
+    console.log(value);
+    console.log("check");
+  };
+  const lastDigits = (value) => {
+    setLast4(value);
+    console.log(value);
+  };
+  const profileHandler = (name) => {
+    setprofname(name);
+  };
+  return (
     <ModalBar>
-      {!loggedIn?(!status ? (
-        <InitialModal phoneOpen={phoneHandler} modalOpen={modalHandler} prof={profileHandler} />
+      {!status ? (
+        <InitialModal
+          phoneOpen={phoneHandler}
+          modalOpen={modalHandler}
+          prof={profileHandler}
+        />
       ) : newState ? (
         <PhoneModal
           digit={digit}
@@ -65,14 +70,15 @@ setprofname(name)
           digitLast={lastDigits}
         />
       ) : (
-        <Otp modalOpen={modalHandler}
-        phoneNum={last4}
-        prev={nextHandler} password={passwordHandler} 
-        userDetails={userPhone}
+        <Otp
+          modalOpen={modalHandler}
+          phoneNum={last4}
+          prev={nextHandler}
+          password={passwordHandler}
+          userDetails={userPhone}
         />
-      ))
-      :<Welcome phoneNum={phone} profile={profName}/>} 
+      )}
     </ModalBar>
   );
 };
-export default LoginModal;
+export default memo(LoginModal);
