9616a54e1f868f881562d76cb423f97d564dd756
Rohit Multani <rohitmultani153@gmail.com>
2022-05-15 13:45:09 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-
-import './App.css';
-
-import React from 'react';
-import NavBar from './components/NavBar';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import Channel from "./components/Pages/Channel/Channel";
+import WatchList from "./components/Pages/WatchList/WatchList";
+import WatchlistProvider from "./Store/WatchListProvider";
+import Home from "./components/Pages/Home/Home";
+import Play from "./components/Pages/Play/Play";
+import PlayListProvider from "./Store/PlayListProvider";
+import YoutubePlay from "./components/Pages/YoutubePlay/YoutubePlay";
+import Search from "./components/Pages/Search/Search";
+import Welcome from "./components/modal/Welcome";
+import Tv from "./components/Pages/Tv/Tv";
+import Movie from "./components/Pages/Movies/Movies";
 function App() {
   return (
-   <div>
-     <NavBar/>
-   </div>
+    <div>
+      <PlayListProvider>
+        <WatchlistProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/WatchList" element={<WatchList />} />
+            <Route path="/channel" element={<Channel />} />
+            <Route path="/Play/*" element={<Play />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/profile" element={<Welcome />} />
+            <Route path="/Tv/:any" element={<Tv />} />
+            <Route path="/movie/:any" element={<Movie />} />
+          </Routes>
+        </WatchlistProvider>
+      </PlayListProvider>
+    </div>
   );
 }
 
