affed234ae74fc254f4f153e65bd976f91a41220
Rohit Multani <rohitmultani153@gmail.com>
2022-05-15 13:42:02 +0530

    playlist context

---
+++ src/Store/PlayList-context.js
@@ -0,0 +1,7 @@
+import React from "react";
+const PlayListContext = React.createContext({
+    movies:[],
+    addMovies:(movies)=>{},
+    removeMovies:(id)=>{}
+}); 
+export default PlayListContext;
