9ff3cc4119fb5131ebafae079e36bb8e8e264a4c
Rohit Multani <rohitmultani153@gmail.com>
2022-05-15 12:18:44 +0530

    another layout designed

---
+++ src/components/Layout/BigLayout.js
@@ -0,0 +1,8 @@
+import classes from './BigLayout.module.css'
+import { memo } from "react";
+const BigLayout=(props)=>{
+    return(<div className={classes.BigContainer}>
+{props.children}
+    </div>)
+}
+export default memo(BigLayout);
