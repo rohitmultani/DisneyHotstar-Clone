d815100bdc50fd6570991e31c82681e8a7d952e3
Rohit Multani <rohitmultani153@gmail.com>
2022-05-15 12:21:22 +0530

    props image added

--- src/components/LayoutDesigned/Design1.js
+++ src/components/LayoutDesigned/Design1.js
@@ -1,12 +1,14 @@
 import LandScape from '../Layout/LandScape';
 import classes from './Design1.module.css';
 import batMan from '../../Images/batman.webp'
-const Design1 =()=>{
+import { memo } from "react";
+const Design1 =(props)=>{
+    const imAddress=`http://image.tmdb.org/t/p/w500/${props.image}`
     return(
         <LandScape>
-            <img src={batMan} className={classes.im}></img>
+            <img src={imAddress } className={classes.im}></img>
         </LandScape>
     );
 
 }
-export default Design1;
+export default memo(Design1);
